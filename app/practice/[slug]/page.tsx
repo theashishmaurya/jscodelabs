e5c181f78bcbdc896cc4563148bdcbed3666516f
AshishIvoyant <ashish.1999vns@gmail.com>
2023-07-22 17:40:04 +0530

    Removed slash

--- app/practice/[slug]/page.tsx
+++ app/practice/[slug]/page.tsx
@@ -2,7 +2,7 @@ import CodeEditor from "@/components/codeEditor";
 import { readdirSync, readFileSync } from "fs";
 import path, { join } from "path";
 
-const DIR = join(process.cwd(), "/questions");
+const DIR = join(process.cwd(), "questions");
 
 export default function Home({ params }: { params: { slug: string } }) {
   // contains all the questions folder
@@ -13,26 +13,29 @@ export default function Home({ params }: { params: { slug: string } }) {
     (folder) => folder === params.slug
   );
 
-  // Read all the files from the folder
+  console.log(questionFolderName);
+
+  // // Read all the files from the folder
 
   const questionFiles = readdirSync(join(DIR, questionFolderName[0]));
+  console.log(questionFiles, DIR + questionFolderName[0]);
 
-  // Read all the files from the template folder
+  // // Read all the files from the template folder
 
   const templateFiles = readdirSync(
     join(DIR, questionFolderName[0], "template")
   );
-  console.log(templateFiles, "This is where it fails");
+  console.log(templateFiles);
 
   return (
     <div>
       {/* <CodeEditor data={"asa"} /> */}
       <div>{params.slug}</div>
       <div>
-        {templateFiles.map((file) => {
+        {templateFiles.map((file, index) => {
           return (
             // eslint-disable-next-line react/jsx-key
-            <div>
+            <div key={index}>
               {readFileSync(
                 join(DIR, questionFolderName[0], "template", file),
                 "utf-8"
