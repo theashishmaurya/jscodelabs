956e3ce3c88ef93946866f1ffd1d01ef48072842
AshishIvoyant <ashish.1999vns@gmail.com>
2023-07-26 01:46:42 +0530

    Removed the console feed as default console works for now

--- components/codeEditor/index.tsx
+++ components/codeEditor/index.tsx
@@ -1,16 +1,13 @@
 "use client";
 import {
-  Sandpack,
   SandpackCodeEditor,
   SandpackConsole,
   SandpackLayout,
   SandpackPreview,
   SandpackProvider,
 } from "@codesandbox/sandpack-react";
-import { LogsContainer } from "./customConsole";
 
 export default function CodeEditor({ files }: { files: any }) {
-  //TODO: Implement the console in the code editor
   //TODO: Implement the resizable panes
   return (
     <SandpackProvider template="react" theme="dark" files={files}>
