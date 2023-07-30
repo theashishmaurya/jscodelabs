9e2c4dbf84f595a5445086b88995353febf6a9c1
AshishIvoyant <ashish.1999vns@gmail.com>
2023-07-30 15:38:03 +0530

    Added Console and Resizable panels

--- components/codeEditor/index.tsx
+++ components/codeEditor/index.tsx
@@ -1,30 +1,211 @@
 "use client";
 import {
+  ConsoleIcon,
+  RoundedButton,
+  Sandpack,
   SandpackCodeEditor,
   SandpackConsole,
   SandpackFileExplorer,
   SandpackLayout,
   SandpackPreview,
   SandpackProvider,
+  SandpackStack,
 } from "@codesandbox/sandpack-react";
+import React, { useState } from "react";
+import classNames from "classnames";
 
 export default function CodeEditor({ files }: { files: any }) {
-  //TODO: Implement the resizable panes
+  const [consoleVisibility, setConsoleVisibility] = React.useState(true);
+  const [counter, setCouter] = useState(0);
+
+  const dragEventTargetRef = React.useRef<any>(null);
+  const [horizontalSize, setHorizontalSize] = React.useState(50); // 50% of the screen
+  const [verticalSize, setVerticalSize] = React.useState(70);
+  const RightColumn = SandpackStack;
+
+  const rightColumnStyle = {
+    flexGrow: 100 - horizontalSize,
+    flexShrink: 100 - horizontalSize,
+    flexBasis: 0,
+    width: 100 + "%",
+    display: "flex",
+    gap: consoleVisibility ? 1 : 0,
+    height: "100%",
+  };
+
+  const topRowStyle = {
+    flexGrow: verticalSize,
+    flexShrink: verticalSize,
+    flexBasis: 0,
+    overflow: "hidden",
+    height: "100%",
+  };
+
+  const rightColumnProps = {
+    className: ".sp" + "-preset-column",
+    style: rightColumnStyle,
+  };
+
+  const actionsChildren = (
+    <ConsoleCounterButton
+      counter={counter}
+      onClick={(): void => setConsoleVisibility((prev) => !prev)}
+    />
+  );
+  const onDragMove = (event: MouseEvent): void => {
+    if (!dragEventTargetRef.current) return;
+
+    const container = dragEventTargetRef.current.parentElement as
+      | HTMLDivElement
+      | undefined;
+
+    if (!container) return;
+
+    const direction = dragEventTargetRef.current.dataset.direction as
+      | "horizontal"
+      | "vertical";
+    const isHorizontal = direction === "horizontal";
+
+    const { left, top, height, width } = container.getBoundingClientRect();
+    const offset = isHorizontal
+      ? ((event.clientX - left) / width) * 100
+      : ((event.clientY - top) / height) * 100;
+    const boundaries = Math.min(Math.max(offset, 25), 75);
+
+    if (isHorizontal) {
+      setHorizontalSize(boundaries);
+    } else {
+      console.log(boundaries);
+      setVerticalSize(boundaries);
+    }
+
+    container.querySelectorAll(`.sp-stack`).forEach((item) => {
+      (item as HTMLDivElement).style.pointerEvents = "none";
+    });
+  };
+
+  const stopDragging = (): void => {
+    const container = dragEventTargetRef.current?.parentElement as
+      | HTMLDivElement
+      | undefined;
+
+    if (!container) return;
+
+    container.querySelectorAll(`.sp-stack`).forEach((item) => {
+      (item as HTMLDivElement).style.pointerEvents = "";
+    });
+
+    dragEventTargetRef.current = null;
+  };
+
+  React.useEffect(() => {
+    document.body.addEventListener("mousemove", onDragMove);
+    document.body.addEventListener("mouseup", stopDragging);
+
+    return (): void => {
+      document.body.removeEventListener("mousemove", onDragMove);
+      document.body.removeEventListener("mouseup", stopDragging);
+    };
+  }, []);
+
+  // React.useEffect(() => {
+  //   setConsoleVisibility(options?.showConsole ?? false);
+  // }, [options.showConsole]);
+
   return (
     <SandpackProvider template="react" theme="dark" files={files}>
-      <SandpackLayout style={{ height: "100vh" }}>
+      <SandpackLayout
+        style={{
+          height: "100vh",
+        }}
+      >
         {/* File explorer is mandatory in react so we can also use it in our code editor */}
-        <SandpackFileExplorer style={{ height: "100vh" }} />
-        <SandpackCodeEditor style={{ height: "100vh" }} />
-        <SandpackPreview
-          style={{ height: "100vh" }}
-          showNavigator={true}
-          showOpenInCodeSandbox={false}
-          showRefreshButton={true}
-          showSandpackErrorOverlay={true}
+        <SandpackFileExplorer style={{ height: "100%" }} />
+        <SandpackCodeEditor
+          style={{
+            height: "100%", // use the original editor height
+            flexGrow: horizontalSize,
+            flexShrink: horizontalSize,
+            flexBasis: 0,
+            overflow: "hidden",
+          }}
         />
-        <SandpackConsole />
+        <div
+          className={classNames("resize-handler", [])}
+          style={{
+            left: `calc(${horizontalSize}% - 5px)`,
+            width: 10,
+            cursor: "ew-resize",
+          }}
+          data-direction="horizontal"
+          onMouseDown={(event): void => {
+            dragEventTargetRef.current = event.target;
+          }}
+        />
+        <RightColumn {...rightColumnProps}>
+          <SandpackPreview
+            actionsChildren={actionsChildren}
+            style={topRowStyle}
+            showNavigator={true}
+            showOpenInCodeSandbox={false}
+            showRefreshButton={true}
+            showSandpackErrorOverlay={true}
+          />
+          <div
+            className={classNames("resize-handler", [
+              // dragHandler({ direction: "vertical" }),
+            ])}
+            data-direction="vertical"
+            onMouseDown={(event): void => {
+              dragEventTargetRef.current = event.target;
+            }}
+            style={{
+              top: `calc(${verticalSize}% - 5px)`,
+              cursor: "ns-resize",
+              width: 100 + "%",
+              height: 10,
+            }}
+          />
+
+          <div
+            className="console-wrapper w-full overflow-hidden"
+            style={{
+              flexGrow: consoleVisibility ? 100 - verticalSize : 0,
+              flexShrink: consoleVisibility ? 100 - verticalSize : 0,
+              flexBasis: 0,
+              width: "100%",
+              maxHeight: consoleVisibility
+                ? `calc(${100 - verticalSize}% - 1px)`
+                : 0,
+            }}
+          >
+            {/* Use better Console */}
+            <SandpackConsole
+              className="h-full overflow-hidden"
+              onLogsChange={(logs) => {
+                setCouter(logs.length);
+              }}
+              showHeader={false}
+            />
+          </div>
+        </RightColumn>
       </SandpackLayout>
     </SandpackProvider>
   );
 }
+
+const ConsoleCounterButton: React.FC<{
+  onClick: () => void;
+  counter: number;
+}> = ({ onClick, counter }) => {
+  return (
+    <RoundedButton className="relative min-w-[12px]" onClick={onClick}>
+      <ConsoleIcon />
+      {/* {counter > 0 && (
+        <strong className=" min-w-12 h-12 px-2 rounded-full text-xs leading-4 absolute top-0  right-0 font-normal p-10">
+          {counter}
+        </strong>
+      )} */}
+    </RoundedButton>
+  );
+};
